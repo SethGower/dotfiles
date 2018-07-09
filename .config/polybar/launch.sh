f4f8da2f93e38681f07f20e6721fe40efd2ab847
Seth Gower <sethzerish@gmail.com>
2018-07-09 14:55:45 -0400

    Adds compatiblility for extended display

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+MONITOR="eDP1" polybar -r top &
+MONITOR="eDP1" polybar -r bottom &
+
+MONITOR="HDMI1" polybar -r top-secondary &
+MONITOR="HDMI2" polybar -r top-secondary &
+
+echo "Bars launched..."
