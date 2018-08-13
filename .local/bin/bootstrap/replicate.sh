83d51bb8ae29563b5a5dd25b84e086f592a4430f
Seth Gower <sethzerish@gmail.com>
2018-08-13 01:53:14 -0400

    fixes install script

--- .local/bin/bootstrap/replicate.sh
+++ .local/bin/bootstrap/replicate.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 deps="git rsync"
 for dep in $deps; do
-	if ! [ "`which $dep > /dev/null 2>&1`" ]; then
+	if [ "$(which $dep >/dev/null 2>&1)" ]; then
 		printf "$0: error: could not find program \"$dep\" in PATH\n" 1>&2
 		exit 1
 	fi
 done
 
 tempfolder="/tmp/dots"
-gitdir="$HOME/.dotfiles"
-repo='git@gitlab.com:rehashedsalt/home.git'
+gitdir="$HOME/.dot"
+repo='git@gitlab.com:SethGower/dotfiles.git'
 config='git --git-dir="$gitdir" --work-tree="$HOME"'
 
 git clone --recursive --separate-git-dir="$gitdir" "$repo" "$tempfolder"
@@ -26,4 +26,5 @@ printf "$0: dotfiles set up successfully\n"
 printf "Be sure and configure the following before issuing any commits:\n"
 printf "    git config --global user.email\n"
 printf "    git config --global user.name\n"
+printf "It is recommended that you create GPG keys and sign commits"
 exit 0
