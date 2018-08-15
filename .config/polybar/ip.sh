25b6efb99af2bc94582aa1df598f723949a64218
Seth Gower <sethzerish@gmail.com>
2018-08-14 21:43:33 -0400

    deletes old ip script, since it is deprecated by the ip-ipc.sh script

--- .config/polybar/ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-public=$(curl --limit-rate 1k -s ipinfo.io/ip)
-private=$(ip a | grep 'wlp4s0' | grep 'inet' | cut -d '/' -f1 | cut -d 't' -f2 | cut -d ' ' -f2)
-
-if [[ "$public" == "$private" ]]; then
-    echo $public
-else
-    echo "$private, $public"
-fi
