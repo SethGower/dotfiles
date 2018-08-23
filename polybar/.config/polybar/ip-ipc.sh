238683d7df2a49dbe68a204210d336a242f16a22
Seth Gower <sethzerish@gmail.com>
2018-08-23 10:04:40 -0400

    removes public or private label if they are the same

--- polybar/.config/polybar/ip-ipc.sh
+++ polybar/.config/polybar/ip-ipc.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 STATE=$(cat /tmp/polybar-ip-state)
 
-if [ $STATE == 'public' ]; then
-	echo Public: $(curl --limit-rate 1k -s ipinfo.io/ip)
-	echo "private" >/tmp/polybar-ip-state
+public=$(curl --limit-rate 1k -s ipinfo.io/ip)
+private=$(ip a | grep $NETWORK_INTERFACE | grep 'inet' | cut -d '/' -f1 | cut -d 't' -f2 | cut -d ' ' -f2)
+
+if [ $public == $private ]; then
+	echo $public
 else
-	echo Private: $(ip a | grep $NETWORK_INTERFACE | grep 'inet' | cut -d '/' -f1 | cut -d 't' -f2 | cut -d ' ' -f2)
-	echo "public" >/tmp/polybar-ip-state
+	if [ $STATE == 'public' ]; then
+		echo Public: $(curl --limit-rate 1k -s ipinfo.io/ip)
+		echo "private" >/tmp/polybar-ip-state
+	else
+		echo Private: $(ip a | grep $NETWORK_INTERFACE | grep 'inet' | cut -d '/' -f1 | cut -d 't' -f2 | cut -d ' ' -f2)
+		echo "public" >/tmp/polybar-ip-state
+	fi
 fi
