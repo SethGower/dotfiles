78e6ab3b5b3d62d88d25ec8f2e081cb1bf5e9d86
Seth Gower <sethzerish@gmail.com>
2019-08-06 17:42:20 -0400

    changes location of wallpapers

--- scripts/.config/scripts/wmstartup.sh
+++ scripts/.config/scripts/wmstartup.sh
@@ -4,16 +4,16 @@ unset PRIMARY_TOP_LEFT PRIMARY_TOP_CENTER PRIMARY_TOP_RIGHT
 unset PRIMARY_BOTTOM_LEFT PRIMARY_BOTTOM_CENTER PRIMARY_BOTTOM_RIGHT
 
 if [[ -x "$HOME/.config/scripts/$(hostname).rc" ]]; then
-	printf "[INFO] Sourcing ~/.config/scripts/$(hostname).rc\n"
-	source $HOME/.config/scripts/$(hostname).rc
+    printf "[INFO] Sourcing ~/.config/scripts/$(hostname).rc\n"
+    source $HOME/.config/scripts/$(hostname).rc
 fi
 
 if [[ $(hostname) == 'odyssey' ]]; then
-	printf "[INFO] Setting HDMI1 to primary monitor\n"
-	xrandr --output HDMI2 --primary
+    printf "[INFO] Setting HDMI1 to primary monitor\n"
+    xrandr --output HDMI2 --primary
 elif [[ $(hostname) == "daedalus" ]]; then
-	printf "[INFO] Setting primary monitor to eDP1 and setting resolution to 1080p\n"
-	xrandr --output eDP1 --mode 1920x1080
+    printf "[INFO] Setting primary monitor to eDP1 and setting resolution to 1080p\n"
+    xrandr --output eDP1 --mode 1920x1080
 fi
 
 # Kill everything
@@ -21,23 +21,23 @@ printf "[INFO] Stopping existing services\n"
 services="polybar compton redshift-gtk"
 
 for service in $services; do
-	printf "  [INFO] Sending signal to all $service\n"
-	killall $service
+    printf "  [INFO] Sending signal to all $service\n"
+    killall $service
 done
 # Wait for them to die
 for service in $services; do
-	i=0
-	while pgrep $service >/dev/null 2>&1; do
-		if [[ "$i" == "10" ]]; then
-			printf "  [INFO] Waited too long, killing $service with prejudice\n"
-			killall -9 $service
-		else
-			printf "  [INFO] Waiting on $service\n"
-			sleep 0.5
-			i=$(expr $i + 1)
-		fi
-	done
-	unset i
+    i=0
+    while pgrep $service >/dev/null 2>&1; do
+        if [[ "$i" == "10" ]]; then
+            printf "  [INFO] Waited too long, killing $service with prejudice\n"
+            killall -9 $service
+        else
+            printf "  [INFO] Waiting on $service\n"
+            sleep 0.5
+            i=$(expr $i + 1)
+        fi
+    done
+    unset i
 done
 
 printf "  [INFO] Killing Caffeine\n"
@@ -62,15 +62,15 @@ caffeine &
 
 # Mons daemon to auto remove external monitors on laptop
 if [[ $(hostname) == 'daedalus' ]]; then
-	printf "    [INFO] Starting mons daemon\n"
-	mons -a >/dev/null 2>&1 &
+    printf "    [INFO] Starting mons daemon\n"
+    mons -a >/dev/null 2>&1 &
 fi
 
 printf "[INFO] Setting Wallpaper...\n"
 
 if which feh >/dev/null 2>&1; then
-	printf "    [INFO] Setting wallpaper with feh"
-	feh --bg-scale $HOME/.dotfiles/background.png
+    printf "    [INFO] Setting wallpaper with feh"
+    feh --bg-scale $HOME/.wallpapers/background.png
 fi
 
 [[ -x /usr/bin/numlockx ]] && numlockx off
