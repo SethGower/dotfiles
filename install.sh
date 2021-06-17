722337fa7ee22a3c3b5a92e819875be94ba4c05e
Seth Gower <sethzerish@gmail.com>
2021-06-17 17:56:15 -0400

    working on the installation scripts

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ -z $@ ]]; then
+  >&2 echo "Usage Error! Please provide modules to install"
+  >&2 echo "Example: ./$0 i3 polybar xorg neovim"
+  exit 1
+fi
+
+export OS=$(cat /etc/os-release | grep -E "\bID\b" | cut -d'=' -f2)
+
+for package in $@
+do
+  ./install.d/${OS}/$package.sh
+  stow $package
+done
