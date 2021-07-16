d21fc3500b494f23ce31e428a1026664f79360e4
Seth Gower <sethzerish@gmail.com>
2021-07-15 22:53:38 -0400

    nvim: adds playground for ts

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -144,6 +144,12 @@ require('packer').startup(function()
     'romgrk/nvim-treesitter-context',
     requires = {'nvim-treesitter/nvim-treesitter'}
   }
+
+  -- Playground for tree-sitter
+  use {
+    'nvim-treesitter/playground',
+    requires = {'nvim-treesitter/nvim-treesitter'}
+  }
 end)
 
 vim.o.termguicolors = true
@@ -313,6 +319,24 @@ ts.setup {
   },
   context = { -- added by romgrk/nvim-treesitter-context
     enable = true,
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
   }
 }
 
