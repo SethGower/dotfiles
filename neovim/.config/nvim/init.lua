1a26136fbf52bce82e9b8e317b45df4fb090d5c5
Seth Gower <sethzerish@gmail.com>
2021-07-16 15:13:16 -0400

    nvim: changes tabs to spaces

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -268,11 +268,11 @@ end
 
 opt.updatetime  = 300
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, {
-		virtual_text = false,
-		underline = true,
-		signs = true,
-	}
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    underline = true,
+    signs = true,
+  }
 )
 
 vim.cmd([[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]])
