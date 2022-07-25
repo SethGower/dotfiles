0154538fedf4c92e749eea7307e3ce55e44213cb
Seth Gower <sethzerish@gmail.com>
2022-07-25 17:51:23 -0400

    dotbot: updates packages for arch and install conf

--- bootstrap/arch.sh
+++ bootstrap/arch.sh
@@ -2,8 +2,9 @@
 cd $SCRIPT_DIR
 source ./arch-functions.sh
 
-install_pkg polybar neovim-nightly-bin alacritty compton-tryone-git dunst git \
-            i3-gaps ranger redshift rofi tmux zsh 
+#install_pkg polybar neovim alacritty dunst git \
+#            i3-gaps ranger redshift rofi tmux zsh \
+#            xfce4 xfce4-goodies
 
 if [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]
 then
