164bbeb4310c94f8de7a346ce6c5bb0df754d619
Seth Gower <sethzerish@gmail.com>
2022-11-22 12:40:04 -0500

    nvim: formats with LSP

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -57,7 +57,8 @@ return require('packer').startup(function()
     use {
         'ygm2/rooter.nvim',
         config = function()
-            vim.g.rooter_pattern = {'.git', 'Makefile', '_darcs', '.hg', '.bzr', '.svn', 'node_modules', 'CMakeLists.txt'}
+            vim.g.rooter_pattern = { '.git', 'Makefile', '_darcs', '.hg', '.bzr', '.svn', 'node_modules',
+                'CMakeLists.txt' }
             vim.g.outermost_root = true
         end
     }
@@ -72,7 +73,7 @@ return require('packer').startup(function()
     use {
         'sakhnik/nvim-gdb',
         run = ':!./install.sh',
-        cmd = {'GdbStart', 'GdbStartLLDB'},
+        cmd = { 'GdbStart', 'GdbStartLLDB' },
     }
     -- treesitter interface for vim
     use {
