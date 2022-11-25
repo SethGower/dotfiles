68c8d47e6fc90422c94d3eb4c03ad57b0118600a
Seth Gower <sethzerish@gmail.com>
2022-11-25 09:20:36 -0500

    meta: updates bootstrap command to add check for the packer directory

--- bootstrap/arch.sh
+++ bootstrap/arch.sh
@@ -5,9 +5,3 @@ source ./arch-functions.sh
 #install_pkg polybar neovim alacritty dunst git \
 #            i3-gaps ranger redshift rofi tmux zsh \
 #            xfce4 xfce4-goodies
-
-if [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]
-then
-  git clone https://github.com/wbthomason/packer.nvim\
-      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
