0abb3634ee3845f8e6e08e66eea72cc7ef13822d
Seth Gower <sethzerish@gmail.com>
2022-12-06 09:15:20 -0500

    nvim: adds vsg_format source to provide code formatting using vsg

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -538,7 +538,24 @@ local vsg_lint = {
     })
 }
 
+local vsg_format = {
+    name = "VSG Formatting",
+    method = null_ls.methods.FORMATTING,
+    filetypes = { "vhdl" },
+    generator = helpers.formatter_factory({
+        command = "vsg",
+        args = { "-c$ROOT/vsg_config.yaml", "-f=$FILENAME", "-of=syntastic", "--fix" },
+        cwd = nil,
+        check_exit_code = { 0, 1 },
+        from_stderr = false,
+        to_temp_file = true,
+        from_temp_file = true,
+        to_stdin = false,
+        multiple_files = false,
+    })
+}
+
 null_ls.setup({
     diagnostics_format = "[#{c}] #{m} (#{s})",
-    sources = vsg_lint
+    sources = { vsg_lint, vsg_format }
 })
