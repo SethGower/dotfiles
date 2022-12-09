8c654d2aab28e2569aaffc900957aeb3d983f499
Seth Gower <sethzerish@gmail.com>
2022-12-09 08:32:44 -0500

    nvim: updates indentline to not show the context. it was annoying

--- config/nvim/lua/plugins/others.lua
+++ config/nvim/lua/plugins/others.lua
@@ -36,8 +36,8 @@ M.indentline = function()
 
     require("indent_blankline").setup {
         space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
+        -- show_current_context = true,
+        -- show_current_context_start = true,
     }
 end
 
