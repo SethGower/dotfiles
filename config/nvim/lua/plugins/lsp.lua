908ef04d1549bd05f8e32af8d08eddffc8951cc5
Seth Gower <sethzerish@gmail.com>
2022-12-12 15:10:59 -0500

    nvim: realigns commas in mappings in lsp and suppresses warning

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ local util = require('lspconfig.util')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -17,19 +17,19 @@ local on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD',         '<cmd>lua vim.lsp.buf.declaration()<CR>',      opts)
-    buf_set_keymap('n', 'gi',         '<cmd>Trouble lsp_implementations<CR>',        opts)
-    buf_set_keymap('n', 'gd',         '<cmd>lua vim.lsp.buf.definition()<CR>',       opts)
-    buf_set_keymap('n', 'gr',         '<cmd>Trouble lsp_references<CR>',             opts)
-    buf_set_keymap('n', '<C-k>',      '<cmd>lua vim.lsp.buf.hover()<CR>',            opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',           opts)
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',      opts)
-    buf_set_keymap('n', '<leader>e',  '<cmd>lua vim.diagnostic.open_float()<CR>',    opts)
-    buf_set_keymap('n', '[d',         '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d',         '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap("n", "<leader>f",  "<cmd>lua vim.lsp.buf.format{async = true}<CR>",       opts)
-    buf_set_keymap("n", "<leader>d",  "<cmd>Trouble document_diagnostics<CR>",       opts)
-    buf_set_keymap("n", "<leader>D",  "<cmd>Trouble workspace_diagnostics<CR>",      opts)
+    buf_set_keymap('n', 'gD',         '<cmd>lua vim.lsp.buf.declaration()<CR>',        opts)
+    buf_set_keymap('n', 'gi',         '<cmd>Trouble lsp_implementations<CR>',          opts)
+    buf_set_keymap('n', 'gd',         '<cmd>lua vim.lsp.buf.definition()<CR>',         opts)
+    buf_set_keymap('n', 'gr',         '<cmd>Trouble lsp_references<CR>',               opts)
+    buf_set_keymap('n', '<C-k>',      '<cmd>lua vim.lsp.buf.hover()<CR>',              opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',             opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',        opts)
+    buf_set_keymap('n', '<leader>e',  '<cmd>lua vim.diagnostic.open_float()<CR>',      opts)
+    buf_set_keymap('n', '[d',         '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',   opts)
+    buf_set_keymap('n', ']d',         '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',   opts)
+    buf_set_keymap("n", "<leader>f",  "<cmd>lua vim.lsp.buf.format{async = true}<CR>", opts)
+    buf_set_keymap("n", "<leader>d",  "<cmd>Trouble document_diagnostics<CR>",         opts)
+    buf_set_keymap("n", "<leader>D",  "<cmd>Trouble workspace_diagnostics<CR>",        opts)
 
       vim.opt.updatetime  = 300
       vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
