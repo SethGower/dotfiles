54f46b3fb8513b479bf39cc9fc622c7b2bdd7e7e
Seth Gower <sethzerish@gmail.com>
2022-12-14 08:56:32 -0500

    nvim: updates sumneko_lua to have the correct lua version (LuaJIT)
    modifies config of lua lsp to specify the lua version that neovim uses.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -76,7 +76,7 @@ M.setup = function()
     -- Use a loop to conveniently call 'setup' on multiple servers and
     -- map buffer local keybindings when the language server attaches
     local servers = { "pylsp", "rust_analyzer", "texlab", "ltex", "yamlls", "svls", "svlangserver", "vhdl_ls", "bashls",
-        "jsonls", "sumneko_lua", "vimls" }
+        "jsonls", "vimls" }
     for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
             on_attach = on_attach,
@@ -98,6 +98,34 @@ M.setup = function()
         },
         flags = {
             debounce_text_changes = 150,
+        }
+    }
+
+    local runtime_path = vim.split(package.path, ";")
+    lspconfig["sumneko_lua"].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT",
+                    -- Setup your lua path
+                    path = runtime_path,
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = { "vim" },
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
         }
     }
 
