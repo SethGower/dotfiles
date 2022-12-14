7170cbc6eb3bdadbe11fd28c022bb9737c9248f8
Seth Gower <sethzerish@gmail.com>
2022-12-14 08:34:23 -0500

    nvim: updates completions to use tab instead of C-n

--- config/nvim/lua/plugins/completions.lua
+++ config/nvim/lua/plugins/completions.lua
@@ -1,5 +1,11 @@
 local cmp = require 'cmp'
+local luasnip = require('luasnip')
 local _ = { behavior = cmp.SelectBehavior.Select }
+
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup {
     snippet = {
@@ -14,7 +20,26 @@ cmp.setup {
         { name = 'path' }
     },
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(nil),
-        ['<C-n>'] = cmp.mapping.select_next_item(nil),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     }
 }
