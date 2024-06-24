11ff20f954af1466159f267ba4d8a8ad62ce07d1
Seth Gower <seth@sethgower.com>
2024-06-24 13:23:50 -0400

    cleans up my tree-sitter config

--- config/nvim/lua/plugins/tree-sitter.lua
+++ config/nvim/lua/plugins/tree-sitter.lua
@@ -18,6 +18,7 @@ local ts = require('nvim-treesitter.configs')
 
 ts.setup {
     ensure_installed = 'all',
+    ignore_install = { 'norg' }, -- this is failing with a cc1plus error and I can't figure it out. I don't need it, so ignore it
     query_linter = {
         enabled = true,
         use_virtual_text = true,
@@ -44,27 +45,6 @@ ts.setup {
     },
     autopairs = {
         enable = false,
-    },
-    -- matchup = {
-    --   enable =false,
-    -- },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<cr>',
-            show_help = '?',
-        },
     },
 }
 
