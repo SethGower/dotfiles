73bf263e1daa34ca764d7e5301098e9aff660eb7
Seth Gower <seth@sethgower.com>
2024-07-01 11:36:09 -0400

    fix(nvim): removes deprecated config things for git-signs

--- config/nvim/lua/plugins/git-signs.lua
+++ config/nvim/lua/plugins/git-signs.lua
@@ -1,12 +1,12 @@
 ------------------------- GIT SIGNS -------------------------
 require('gitsigns').setup {
     signs = {
-        add          = { hl = 'GitSignsAdd',    text = '│', numhl = 'GitSignsAddNr',    linehl = 'GitSignsAddLn' },
-        change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete       = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete    = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        untracked    = { hl = 'GitSignsAdd',    text = '┆', numhl = 'GitSignsAddNr',    linehl = 'GitSignsAddLn' },
+        add          = { text = '│' },
+        change       = { text = '│' },
+        delete       = { text = '│' },
+        topdelete    = { text = '│' },
+        changedelete = { text = '│' },
+        untracked    = { text = '┆' },
     },
     current_line_blame = true,
     -- current_line_blame_delay = 100
