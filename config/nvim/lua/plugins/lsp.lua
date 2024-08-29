868180a2a8aa6ea29c6946e0c9dc845c55c23418
Seth Gower <seth@sethgower.com>
2024-08-29 09:41:15 -0400

    adds lsp highlighting to highlight other occurences of a symbol

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,25 @@ M.on_attach = function (client, bufnr)
     vim.opt.updatetime = 300
 
     require('nlspsettings').update_settings(client.name)
+
+    -- Server capabilities spec:
+    -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#serverCapabilities
+    if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+        vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+        vim.api.nvim_create_autocmd("CursorHold", {
+            callback = vim.lsp.buf.document_highlight,
+            buffer = bufnr,
+            group = "lsp_document_highlight",
+            desc = "Document Highlight",
+        })
+        vim.api.nvim_create_autocmd("CursorMoved", {
+            callback = vim.lsp.buf.clear_references,
+            buffer = bufnr,
+            group = "lsp_document_highlight",
+            desc = "Clear All the References",
+        })
+    end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
