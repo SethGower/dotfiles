c659fb7b6c9e82981026e2d2f7d5a79ab628d6f3
Seth Gower <seth@sethgower.com>
2025-09-01 07:51:54 -0400

    feat(nvim): switches the tree-sitter context to be configured properly
    seems that the config for TS context moved, this configures it properly

--- config/nvim/lua/plugins/tree-sitter.lua
+++ config/nvim/lua/plugins/tree-sitter.lua
@@ -29,14 +29,25 @@ ts.setup {
     autotag = {                -- added by windwp/nvim-ts-autotag
         enable = true,
     },
-    context = { -- added by romgrk/nvim-treesitter-context
-        enable = true,
-    },
     autopairs = {
         enable = false,
     },
 }
-
+require 'treesitter-context'.setup {
+    enable = true,          -- Enable this plugin (Can be enabled/disabled later via commands)
+    multiwindow = false,    -- Enable multiwindow support.
+    max_lines = 3,          -- How many lines the window should span. Values <= 0 mean no limit.
+    min_window_height = 0,  -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = 'outer',   -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    mode = 'cursor',        -- Line used to calculate context. Choices: 'cursor', 'topline'
+    -- Separator between context and content. Should be a single character string, like '-'.
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = nil,
+    zindex = 20,   -- The Z-index of the context window
+    on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
 require('ts_context_commentstring').setup {
     enable_autocmd = false,
 }
