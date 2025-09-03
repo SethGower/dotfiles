330c39aa4eb1e308a8c653b382c2d0ff07cc839b
Seth Gower <seth@sethgower.com>
2025-09-03 01:40:52 -0400

    adds external ld loader to handle binaries outside of nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -137,4 +137,10 @@
 
   services.fprintd.enable = true;
   security.pam.services.sudo.fprintAuth = true;
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 }
