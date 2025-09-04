aa524dcb10d86264ac5ecf20783511fdba479243
Seth Gower <seth@sethgower.com>
2025-09-03 20:49:51 -0400

    updates the bootloader config to add a manual entry
    booting into Arch wasn't working, it was actually just booting into
    NixOS.... so manually configuring the alternate config based on what I
    had in my saved Arch GRUB config

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -20,21 +20,21 @@
         # device = "/dev/disk/by-uuid/2159-BBFE";
         device = "nodev";
         efiSupport = true;
-        useOSProber = true;
-        # extraEntries = ''
-        #   menuentry 'Arch Linux' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'gnulinux-simple-42077f60-1a8b-4e1a-9b0f-56160e7caa78' {
-        #   	load_video
-        #   	set gfxpayload=keep
-        #   	insmod gzio
-        #   	insmod part_msdos
-        #   	insmod fat
-        #   	search --no-floppy --fs-uuid --set=root 2159-BBFE
-        #   	echo	'Loading Linux linux-lts ...'
-        #   	linux	/vmlinuz-linux-lts root=/dev/mapper/MyVolGroup-root rw  loglevel=3 cryptdevice=UUID=737b62a4-7676-47f3-80e6-740b622adef6:cryptlvm root=/dev/MyVolGroup/root ibt=off resume=UUID=e1792b40-1d52-4d31-a734-173c82d15dc9 nvidia_drm.modeset=1 mem_sleep_default=deep splash
-        #   	echo	'Loading initial ramdisk ...'
-        #   	initrd	/intel-ucode.img /initramfs-linux-lts.img
-        #   }
-        # '';
+        # useOSProber = true;
+        extraEntries = ''
+          menuentry 'Arch Linux' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'gnulinux-simple-42077f60-1a8b-4e1a-9b0f-56160e7caa78' {
+          	load_video
+          	set gfxpayload=keep
+          	insmod gzio
+          	insmod part_msdos
+          	insmod fat
+          	search --no-floppy --fs-uuid --set=root 2159-BBFE
+          	echo	'Loading Linux linux-lts ...'
+          	linux	/vmlinuz-linux-lts root=/dev/mapper/MyVolGroup-root rw  loglevel=3 cryptdevice=UUID=737b62a4-7676-47f3-80e6-740b622adef6:cryptlvm root=/dev/MyVolGroup/root ibt=off resume=UUID=e1792b40-1d52-4d31-a734-173c82d15dc9 nvidia_drm.modeset=1 mem_sleep_default=deep splash
+          	echo	'Loading initial ramdisk ...'
+          	initrd	/intel-ucode.img /initramfs-linux-lts.img
+          }
+        '';
       };
     };
     initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/737b62a4-7676-47f3-80e6-740b622adef6";
