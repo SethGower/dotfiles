9d495cb8800fedc3818c089722e0200e113addfb
Seth Gower <seth@sethgower.com>
2025-09-03 22:58:05 -0400

    feat(nixos): enables steam

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -20,7 +20,19 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "discord"
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+      "steam-run"
     ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
