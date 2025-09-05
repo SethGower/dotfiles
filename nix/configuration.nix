8f676998e54e43dfe749f9ace63ae51aea1a6962
Seth Gower <seth@sethgower.com>
2025-09-05 00:15:28 -0400

    nixos: updates firefox/librewolf config to add policies
    working on updating the librewolf configuration to have a policy
    definition to have some plugins installed by default. Doesn't seem to be
    working, need to work on it more

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -110,7 +110,42 @@
   programs.firefox = {
     enable = true;
     package = pkgs.librewolf-bin;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/en-US/firefox/addon/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "gt@giphy.com" = {
+          install_url = "https://addons.mozilla.org/en-US/firefox/addon/giphy-for-firefox/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
   };
+
+  environment.etc."firefox/policies/policies.json".target = "librewolf/policies/policies.json";
 
   # programs.element-desktop.enable = true;
 
