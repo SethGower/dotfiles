ff1badae083773327b54bb16daad17f32861c612
Seth Gower <seth@sethgower.com>
2025-10-09 10:36:23 -0400

    updates wezterm to use iosevka term

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,6 +3,11 @@ local config = wezterm.config_builder()
 
 config.default_prog = { 'zellij', '-l', 'welcome' }
 
+config.font = wezterm.font(
+    'IosevkaTerm NerdFont Mono',
+    { stretch = 'Expanded', weight = 'Regular' }
+)
+
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
