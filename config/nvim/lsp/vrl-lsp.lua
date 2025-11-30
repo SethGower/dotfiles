957044ba0d5f0b2f5e7001f01a01951a8140a73a
Seth Gower <seth@sethgower.com>
2025-11-30 17:01:15 -0500

    nvim: adds an lsp definition for vrl-lsp

---
+++ config/nvim/lsp/vrl-lsp.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+--- https://github.com/arshiyasolei/vrl-lsp/tree/master
+---
+--- Language Server for Vector Remap Language (VRL).
+
+---@type vim.lsp.Config
+return {
+  cmd = { 'vrl-lsp' },
+  filetypes = { 'vrl' },
+}
